c0abff6ec0b76eccc44ca9f47ee4e6b49debb5c1
jiaecai <jiaecai@qq.com>
2017-03-24 04:29:38 +0800

    update

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -72,6 +72,7 @@ class SiteController extends Controller
      */
     public function actionIndex()
     {
+        ini_set("display_errors", "On");
         return $this->render('index');
     }
 
