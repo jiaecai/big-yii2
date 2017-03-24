0d3602a00e755529e10d439d7a91d303ccae2297
jiaecai <jiaecai@qq.com>
2017-03-24 23:05:15 +0800

    update

--- frontend/views/wx/index.php
+++ frontend/views/wx/index.php
@@ -8,7 +8,7 @@ $this->title = 'My Yii Application';
 
     <div class="jumbotron">
         <h1>Congratulations!</h1>
-        <p><?=$openId;?></p>
+        <p>openid=<?=$openId;?></p>
 
         <p class="lead">You have successfully created your Yii-powered application.</p>
 
